bfac979688ae226123305941db26f13eadb8ee68
Vincenzo <laveneziana.vincenzo@gmail.com>
2020-04-17 22:48:36 +0200

    hide-show mic and paper-plane complete

--- js/main.js
+++ js/main.js
@@ -1,4 +1,45 @@
 $(document).ready(function () {
-  console.log("andiamo");
   
+
+  var list =$("#msg");
+  var newInput = $(".add_element");
+  var icon = $(".chat_send i");
+
+
+  newInput.keyup(function(event){
+    //console.log(event.which);
+    if(event.which !== 32){
+      toggleMic();
+    }
+    
+    if(event.which == 13) {
+      var text = newInput.val().trim();
+      console.log("ok");
+      
+      if(text !== "") {
+        console.log("ok");
+        sendMsg(text);
+        toggleMicReverse();
+        newInput.val("");
+      }
+    }
+
+  });
+  
+  function sendMsg(word) { 
+    var elementNew = $(".template li").clone();
+    elementNew.prepend('<p> ' + word + " <span class='data_time'>15.30</span>" + '</p>');
+    elementNew.appendTo(list);
+    list.scrollTop(list.innerHeight());
+  }
+
+  function toggleMic() {
+    $('.rec').hide();
+    $('.send').show();
+  }
+
+  function toggleMicReverse() {
+    $('.rec').show();
+    $('.send').hide();
+  }
 });
