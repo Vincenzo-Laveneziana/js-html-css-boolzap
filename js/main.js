4df11a6150c29f9fc94f7a383d566e27396f3c89
Vincenzo <laveneziana.vincenzo@gmail.com>
2020-04-18 12:40:05 +0200

    Updated main conversation ^ _ ^, added click function and dynamic time update

--- js/main.js
+++ js/main.js
@@ -4,34 +4,49 @@ $(document).ready(function () {
   var list =$("#msg");
   var newInput = $(".add_element");
   var icon = $(".chat_send i");
+  var text = newInput.val().trim();
+  
 
 
+  //al click
+  icon.click(function(){
+    text = newInput.val().trim();
+    if(text !== "") { 
+      writeMsg(text)
+    }
+  })
+
+
+  //alla pressione tasto invio
   newInput.keyup(function(event){
-    var text = newInput.val().trim();
+    text = newInput.val().trim();
 
     //console.log(event.which);
     if(event.which !== 32){
       if(text !== ""){
         toggleMic();
-        console.log("ok2");
       }
     } 
     
     if(event.which == 13) {
       if(text !== "") { 
-        sendMsg(text);
-        console.log(text);
-        toggleMicReverse();
-        newInput.val("");
+        writeMsg(text)
       }
     }
+  });//fine key up
 
-  });
+  
+
+  function writeMsg(word){
+    sendMsg(word);
+    //console.log(word);
+    toggleMicReverse();
+    newInput.val("");
+  }
   
   function sendMsg(word) { 
- 
-    var elementNew = $(".template li").clone();
-    elementNew.prepend('<p> ' + word + " <span class='data_time'>15.30</span>" + '</p>');
+    var elementNew = $(".template .posted").clone();
+    elementNew.prepend("<p>" + word + "<span class='data_time'>"+ addData() +"</span>" + "</p>");
     elementNew.appendTo(list);
     list.scrollTop(list.innerHeight());
     //console.log(elementNew.text());
@@ -45,5 +60,12 @@ $(document).ready(function () {
   function toggleMicReverse() {
     $('.rec').show();
     $('.send').hide();
+  }
+
+  function addData(){
+    var myDate = new Date();
+    var hour = myDate.getHours();
+    var minutes = myDate.getMinutes();
+    return hour = hour + ":" + minutes;
   }
 });
